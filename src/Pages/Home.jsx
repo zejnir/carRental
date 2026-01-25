229b97902feddfb390974a95c4177039db5d77ad
Ajdin Zejnirovic <zejnirphoto@gmail.com>
2026-01-25 20:17:49 +0100

    Refactor About and Contact pages; remove unused components and add new AboutSection and Contact components

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,26 +1,32 @@
 import Hero from "../components/Hero";
-import BookCar from "../components/BookCar";
+// import BookCar from "../components/BookCar";
 import PlanTrip from "../components/PlanTrip";
 import PickCar from "../components/PickCar";
-import Banner from "../components/Banner";
+// import Banner from "../components/Banner";
 import ChooseUs from "../components/ChooseUs";
-import Testimonials from "../components/Testimonials";
-import Faq from "../components/Faq";
-import Download from "../components/Download";
+import AboutSection from "../components/About";
+// import Testimonials from "../components/Testimonials";
+// import Faq from "../components/Faq";
+// import Download from "../components/Download";
+import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
 function Home() {
   return (
     <>
       <Hero />
-      <BookCar />
-      <PlanTrip />
-      <PickCar />
-      <Banner />
+      {/* <BookCar /> */}
       <ChooseUs />
-      <Testimonials />
-      <Faq />
-      <Download />
+      <PickCar />
+      <PlanTrip />
+      {/* <Banner /> */}
+      {/* <Testimonials /> */}
+      {/* <Faq /> */}
+      {/* <Download /> */}
+
+      {/* TODO: Add embeded map on contact section*/}
+      <Contact />
+      <AboutSection />
       <Footer />
     </>
   );
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,9 @@
-import Footer from "../components/Footer";
-import HeroPages from "../components/HeroPages";
-import PlanTrip from "../components/PlanTrip";
 import AboutMain from "../images/about/about-main.jpg";
 import Box1 from "../images/about/icon1.png";
 import Box2 from "../images/about/icon2.png";
 import Box3 from "../images/about/icon3.png";
 
-function About() {
+function AboutSection() {
   return (
     <>
       <section className="about-page">
@@ -58,4 +55,4 @@ function About() {
   );
 }
 
-export default About;
+export default AboutSection;
